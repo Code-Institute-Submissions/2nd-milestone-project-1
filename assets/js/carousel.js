24596f91c37ee1b871991412f2a61682b6452746
Ricardo van Noort <ricardo.van.noort@hva.nl>
2020-02-10 15:13:45 +0100

    Added image carousel

---
+++ assets/js/carousel.js
@@ -0,0 +1,29 @@
+// Image carousel
+$(function() {
+  $(".owl-carousel").owlCarousel({
+    items: 5,
+    autoplay: true,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    responsive: {
+      // breakpoint from 0 up
+      0: {
+        items: 2
+      },
+      // breakpoint from 480 up
+      480: {
+        items: 3
+      },
+      // breakpoint from 768 up
+      768: {
+        items: 5
+      }
+    }
+  });
+});
