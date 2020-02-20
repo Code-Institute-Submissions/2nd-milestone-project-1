ff7decfa185521256d77a25569e31b8fc7c2cf3b
Ricardo van Noort <ricardo.van.noort@hva.nl>
2020-02-20 10:52:05 +0100

    Added markers based on json file to Google Maps

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -4,4 +4,18 @@ function initMap() {
     center: { lat: 51.133481, lng: 10.018343 },
     zoom: 4
   });
+
+  $.getJSON("assets/data/data-package-1.json", function(data) {
+    $.each(data, function(key, value) {
+      var latlng = new google.maps.LatLng(
+        value.Location.Latitude,
+        value.Location.Longitude
+      );
+      var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: value.HouseType + " | " + value.MaxPersons + " personen"
+      });
+    });
+  });
 }
